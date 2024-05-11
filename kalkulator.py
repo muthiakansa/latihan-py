f0f0ad2d6dd994fc1ec57c58cfb74c02feae22af
muthiakansa <kansamuthia98@gmail.com>
2024-05-11 16:31:51 +0700

    kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,13 @@
+#a = 100
+#b = 10
+a=int(input("masukan nilai a:"))
+b=int(input("masukan harga b:"))
+hasil_jumlah = a + b
+hasil_kurang = a - b
+hasil_kali = a * b
+hasil_bagi = a / b
+
+print("hasil penjumlahan " + str(hasil_jumlah))
+print("hasil pengurangan " + str(hasil_kurang))
+print("hasil perkalian " + str(hasil_kali))
+print("hasil pembagian " + str(hasil_bagi))
